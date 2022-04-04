f24dee9ac56fa833b2979a2b83e021cb422f06c1
Jan Horodecki <janekhorodecki@gmail.com>
2022-04-04 14:23:13 +0200

    Wojna

--- Programowanie/Java/Marzec/pliki/src/Archives.java
+++ Programowanie/Java/Marzec/pliki/src/Archives.java
@@ -2,9 +2,13 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 public class Archives {
@@ -27,6 +31,55 @@ public class Archives {
                 }
                 fis.close();
             }
+            zos.close();
+            fos.close();
         }
+    }
+
+    public void unpack(String archName, String outDir, String filesList) throws IOException{
+        Path path = Paths.get(outDir);
+        if(!Files.exists(path)){
+            System.out.println("Docelowy katalog wypakowania nie istnieje");
+            Files.createDirectory(path);
+        }
+        File dstDir = new File(outDir);
+        byte buffer[] = new byte[1024];
+        try {
+            FileInputStream fis = new FileInputStream(archName+".zip");
+            ZipInputStream zis = new ZipInputStream(fis);
+            ZipEntry entry = zis.getNextEntry();
+            while (entry != null){
+                File newFile = newFile(dstDir,entry);
+                if (entry.isDirectory()){
+                    if (!newFile.isDirectory() && !newFile.mkdirs()){
+                        throw new IOException("Błąd w tworzeniu katalogu"+newFile);
+                    }
+                }else {
+
+                }
+
+                FileOutputStream fos = new FileOutputStream(newFile);
+                int lenght;
+                while ((lenght = zis.read(buffer))>0){
+                    fos.write(buffer,0,lenght);
+                }
+                fos.close();
+                entry = zis.getNextEntry();
+            }
+            zis.closeEntry();
+            zis.close();
+        }catch (Exception ex){
+            System.out.println("Nieodnaleziono plik"+archName);
+        }
+    }
+
+    private File newFile(File dstDir, ZipEntry entry) throws IOException{
+        File destFile = new File(dstDir,entry.getName());
+        String dstDirPath = dstDir.getCanonicalPath();
+        String dstFilePath = destFile.getCanonicalPath();
+        if (!dstFilePath.startsWith(dstDirPath+File.separator)){
+            throw new IOException("Wpis jest poza docelowym katalogiem "+entry.getName());
+        }
+        return destFile;
     }
 }
--- Programowanie/Java/Marzec/pliki/src/Main.java
+++ Programowanie/Java/Marzec/pliki/src/Main.java
@@ -2,17 +2,26 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String plikRW = "nowyplik.txt";
+        String plikRW = "nowyPlik.txt";
 
         SaveFile f = new SaveFile();
 
         f.saveFile(plikRW,"Umiem zapisywac");
-        f.saveFile("drugiplik","dawdadawdwadadaw");
+        f.saveFile("drugiPlik","dawdadawdwadadaw");
 
         System.out.println("Odczyt pliku #1 " + plikRW);
         System.out.println(f.readFile(plikRW));
 
         System.out.println("odczyt pliku drugi plik ");
-        System.out.println(f.readFile("drugiplik"));
+        System.out.println(f.readFile("drugiPlik"));
+
+        Archives a = new Archives();
+        String plikRO = "drugiPlik";
+        String archName = "noweArchiwum";
+
+        a.pack(archName,plikRO);
+        a.pack(archName+"Multi",plikRO+","+plikRW);
+
+        a.unpack(archName+"Multi","./unpack2TP",plikRW);
     }
 }
